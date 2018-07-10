98b9c1b76244b6d5000e58d7c8624f8eb4ee9020
ly ii fan <35133018+lyifna@users.noreply.github.com>
2018-07-10 17:42:15 +0800

    Add files via upload

---
+++ js/mystorage.js
@@ -0,0 +1,22 @@
+;(function(){
+	window.ms = {
+		set: set,             //localStorage的封装
+		get: get,
+	};
+	
+	function set(key,val){
+		localStorage.setItem(key,JSON.stringify(val));
+		
+	}
+	
+	function get(key){
+		var json=localStorage.getItem(key);
+		if(json){
+			return JSON.parse(json);
+		}
+	}
+})();
+//自存自取
+//ms.set('name','喵喵喵');
+//var name=ms.get('name');
+//console.log('name:',name)
